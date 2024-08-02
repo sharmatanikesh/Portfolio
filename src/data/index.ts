2a92a51aa3ede5c77369cd7a5ef1a2ddc6bb8d68
sharmatanikesh <sharmatanikesh@gmail.com>
2024-08-02 20:22:14 +0530

    Home Page Done

---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    { name: "About Me", link: "#about" },
+    { name: "Skills", link: "#skills" },
+    { name: "Project", link: "#project" },
+    { name: "Contact Me", link: "#contact" },
+  ];
